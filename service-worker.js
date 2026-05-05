ace912d01775fc1ce9ce5b711dabe73324cb30ee
saumyabhartikarn <saumyabhartikarn@gmail.com>
2026-05-05 14:20:54 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "kavita-app-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll([
+        "index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
+});
